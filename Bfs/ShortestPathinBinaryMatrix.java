c14db8d9fb74aad20b17c3807d82f065f14a7924
sakshisingh301 <sakshisingh03aug.ss@gmail.com>
2026-02-08 21:19:29 -0800

    G prep

--- Bfs/ShortestPathinBinaryMatrix.java
+++ Bfs/ShortestPathinBinaryMatrix.java
@@ -41,7 +41,47 @@ public class ShortestPathinBinaryMatrix {
 
     }
 
+
+
+    public int shortestPathBinaryMatrixAgain(int[][] grid) {
+
+        Queue<int [] > queue=new LinkedList<>();
+        int row= grid.length;
+        int column=grid[0].length;
+        boolean [] [] visited=new boolean[row][column];
+
+        if(grid[0][0]!=0 || grid[row-1][column-1]!=0) return -1;
+        queue.add(new int []{0,0,1});
+        visited[0][0]=true;
+        while(!queue.isEmpty())
+        {
+            int [] curr= queue.poll();
+            int nRow=curr[0];
+            int nCol=curr[1];
+            int nPath=curr[2];
+            if(nRow==row-1 && nCol==column-1) return nPath;
+
+            int [] []dir={{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1}};
+            for(int [] d: dir)
+            {
+                int cRow=nRow+d[0];
+                int cCol=nCol+d[1];
+                if(cRow>=0 && cRow<row && cCol>=0 && cCol<column && grid[cRow][cCol]==0 && !visited[cRow][cCol])
+                {
+                    visited[cRow][cCol]=true;
+                    queue.add(new int[]{cRow,cCol,nPath+1});
+
+                }
+            }
+        }
+        return -1;
+
+
+    }
+
     public static void main(String[] args) {
+
+        //grid = [[0,0,0],[1,1,0],[1,1,0]]
 
     }
 }
